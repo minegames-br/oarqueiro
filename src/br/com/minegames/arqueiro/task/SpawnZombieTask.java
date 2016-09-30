4a683ef549b8e041b9ee92d23ba1f46e15f7500b
Joao Emilio <joaoemilio@gmail.com>
2016-09-30 16:26:25 -0300

    https://minegames.atlassian.net/browse/MLA-23

--- src/br/com/minegames/arqueiro/task/SpawnZombieTask.java
+++ src/br/com/minegames/arqueiro/task/SpawnZombieTask.java
@@ -6,14 +6,15 @@ import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Zombie;
+import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 import org.bukkit.scheduler.BukkitRunnable;
 
 import br.com.minegames.arqueiro.GameController;
 import br.com.minegames.arqueiro.domain.Archer;
 import br.com.minegames.arqueiro.domain.target.ZombieTarget;
+import br.com.minegames.logging.Logger;
 import br.com.minegames.util.Utils;
-import zedly.zenchantments.Utilities;
 
 public class SpawnZombieTask extends BukkitRunnable {
 	
@@ -30,24 +31,21 @@ public class SpawnZombieTask extends BukkitRunnable {
     		Bukkit.getConsoleSender().sendMessage(Utils.color("&6Spawning Zombie"));
     		Zombie zombie = spawnZombie();
     	}
-        
     }
     
     private Zombie spawnZombie() {
     	Location l = controller.getRandomSpawnLocationForGroundEnemy();
     	Zombie entity = (Zombie)controller.getWorld().spawnEntity( l , EntityType.ZOMBIE);
-    	
     	int index = new Random().nextInt(controller.getLivePlayers().size());
     	Archer archer = (Archer)controller.getLivePlayers().toArray()[index];
+    	Logger.log("index = " + index + " live players: " + controller.getLivePlayers().size() + archer.getPlayer().getName());
     	entity.setTarget(archer.getPlayer());
     	controller.addEntityTarget(new ZombieTarget(entity));
-		Utilities.addPotion(entity, PotionEffectType.SPEED, 10000, controller.getGame().getLevel().getLevel());
+    	 	
+    	if( !entity.isBaby() ) {
+    		entity.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 10000, controller.getGame().getLevel().getLevel()));
+    	}
     	return entity;
     }
     
-    public static void main(String args[]) {
-    	for(int i = 0; i < 100; i++) {
-    		System.out.println(new Random().nextInt(4));
-    	}
-    }
 }
